0fe5e948d983d7cfabfab676eae2b0c73d1718f6
Asrtodex <dev.mirsuma@gmail.com>
2022-11-03 08:06:02 +0700

    Backup all my dot files

---
+++ .config/nvim/lua/mapping-config.lua
@@ -0,0 +1,79 @@
+local function map(m, k, v)
+    vim.keymap.set(m, k, v, { silent = true })
+end
+
+-- Mimic shell movements
+map('i', '<C-E>', '<ESC>A')
+map('i', '<C-A>', '<ESC>I')
+
+-- Load recent sessions
+map('n', '<leader>sl', '<CMD>SessionLoad<CR>')
+
+-- Keybindings for telescope
+map('n', '<leader>fr', '<CMD>Telescope oldfiles<CR>')
+map('n', '<leader>ff', '<CMD>Telescope find_files<CR>')
+map('n', '<leader>fb', '<CMD>Telescope file_browser<CR>')
+map('n', '<leader>fw', '<CMD>Telescope live_grep<CR>')
+map('n', '<leader>ht', '<CMD>Telescope colorscheme<CR>')
+map('n', '<leader>fp',"<CMD>lua require'telescope'.extensions.project.project{}<CR>")
+
+-- Remap splits navigation to just CTRL + hjkl
+map('n', '<C-h>', '<C-w>h')
+map('n', '<C-j>', '<C-w>j')
+map('n', '<C-k>', '<C-w>k')
+map('n', '<C-l>', '<C-w>l')
+
+-- Make adjusing split sizes a bit more friendly
+map('n', '<C-8>', '<CMD>vertical resize +3<CR>') --some how doesnot work
+map('n', '<C-9>', '<CMD>vertical resize -3<CR>')
+map('n', '<C-0>', '<CMD>resize +3<CR>')
+map('n', '<C-->', '<CMD>resize -3<CR>')
+
+-- Change 2 split windows from vert to horiz or horiz to vert
+map('n', '<Leader>th', '<C-w>t<C-w>H')
+map('n', '<Leader>tk', '<C-w>t<C-w>K')
+
+-- Bar bar
+-- Move to previous/next
+map('n', '<A-,>', '<Cmd>BufferPrevious<CR>')
+map('n', '<A-.>', '<Cmd>BufferNext<CR>')
+-- Re-order to previous/next
+map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>')
+map('n', '<A->>', '<Cmd>BufferMoveNext<CR>')
+-- Goto buffer in position...
+map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>')
+map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>')
+map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>')
+map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>')
+map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>')
+map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>')
+map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>')
+map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>')
+map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>')
+map('n', '<A-0>', '<Cmd>BufferLast<CR>')
+-- Pin/unpin buffer
+map('n', '<A-p>', '<Cmd>BufferPin<CR>')
+-- Close buffer
+map('n', '<A-c>', '<Cmd>BufferClose<CR>')
+-- Wipeout buffer
+--                 :BufferWipeout
+-- Close commands
+--                 :BufferCloseAllButCurrent
+--                 :BufferCloseAllButPinned
+--                 :BufferCloseAllButCurrentOrPinned
+--                 :BufferCloseBuffersLeft
+--                 :BufferCloseBuffersRight
+-- Magic buffer-picking mode
+map('n', '<C-p>', '<Cmd>BufferPick<CR>')
+-- Sort automatically by...
+map('n', '<leader>bb', '<Cmd>BufferOrderByBufferNumber<CR>')
+map('n', '<leader>bd', '<Cmd>BufferOrderByDirectory<CR>')
+map('n', '<leader>bl', '<Cmd>BufferOrderByLanguage<CR>')
+map('n', '<leader>bw', '<Cmd>BufferOrderByWindowNumber<CR>')
+
+-- Other:
+-- :BarbarEnable - enables barbar (enabled by default)
+-- :BarbarDisable - very bad command, should never be used
+
+--NVIM TREE
+map('n', '<leader>b', '<Cmd>NvimTreeFocus<CR>')
---
+++ .config/nvim/lua/themes-config.lua
@@ -0,0 +1,2 @@
+--local ok, _ = pcall(vim.cmd, 'colorscheme onedark')
+local ok, _ = pcall(vim.cmd, 'colorscheme onedarkpro')
